8958100f6e7e0172c5451385057360512e37438c
zfguo@interjoy.com.cn <zfguo@interjoy.com.cn>
2017-06-26 17:27:37 +0800

    SKEyeSDK.framework

--- SKEyeSDKDemo/SKEyeSDKDemo/Libs/SKEyeSDK.framework/Headers/eyeTool.h
+++ SKEyeSDKDemo/SKEyeSDKDemo/Libs/SKEyeSDK.framework/Headers/eyeTool.h
@@ -12,11 +12,11 @@
 FOUNDATION_EXPORT double SKEyeSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char SKEyeSDKVersionString[];
 
-// *****服务名:SK_SERVICE_NAME
+// --- 服务名:SK_SERVICE_NAME ---
 typedef NSString SK_SERVICE_NAME;
 extern NSString *SERVICE_NAME_OBJECT;    // 常见物品识别服务名
 extern NSString *SERVICE_NAME_FRUITS;    // 水果识别服务名
-// ************************************
+
 
 @interface eyeTool : NSObject
 
@@ -29,24 +29,24 @@ extern NSString *SERVICE_NAME_FRUITS;    // 水果识别服务名
  * 上传图片数据
  * 回调方式
  **/
-+ (void)SKEyeSDK_Image:(UIImage *)image service_name:(SK_SERVICE_NAME*)server_name callBack:(void(^)(id responseObject))callBack;
++ (void)SKEyeSDK_Image:(UIImage *)image service_name:(SK_SERVICE_NAME*)serviceName callBack:(void(^)(id responseObject))callBack;
 
 /*
  * 上传本地路径或者URL指向图片
  * 回调方式
  **/
-+ (void)SKEyeSDK_ImagePath:(NSString *)imagePath service_name:(SK_SERVICE_NAME*)server_name callBack:(void(^)(id responseObject))callBack;
++ (void)SKEyeSDK_ImagePath:(NSString *)imagePath service_name:(SK_SERVICE_NAME*)serviceName callBack:(void(^)(id responseObject))callBack;
 
 /*
  * 上传图片数据
  * 非回调方式
  **/
-+ (NSData *)SKEyeSDK_Image:(UIImage *)image service_name:(SK_SERVICE_NAME*)server_name;
++ (NSData *)SKEyeSDK_Image:(UIImage *)image service_name:(SK_SERVICE_NAME*)serviceName;
 
 /*
  * 上传本地路径或者URL指向图片
  * 非回调方式
  **/
-+ (NSData *)SKEyeSDK_ImagePath:(NSString *)imagePath service_name:(SK_SERVICE_NAME*)server_name;
++ (NSData *)SKEyeSDK_ImagePath:(NSString *)imagePath service_name:(SK_SERVICE_NAME*)serviceName;
 
 @end
